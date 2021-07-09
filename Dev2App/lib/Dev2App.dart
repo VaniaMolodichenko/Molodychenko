6756c6fba1918980df5c00955fdfac0fceecdca3
Ivan Molodychenko <ivan.molodychenko@onseo.biz>
2021-07-09 15:52:45 +0300

    create project

---
+++ Dev2App/lib/Dev2App.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library Dev2App;
+
+export 'src/Dev2App_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
